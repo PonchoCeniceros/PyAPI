aad1aa7354cbc19b7d123bcf996f5365eb19e5ed
PonchoCeniceros <gio.icc.uach@gmail.com>
2021-07-08 20:09:37 -0600

    [init] configuration for production mode.

--- project/settings/local.py
+++ project/settings/local.py
@@ -1,12 +1,18 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
+#
+# DEBUG FLAG
+#
 DEBUG = True
 
+#
+# ALLOWED HOSTS
+#
 ALLOWED_HOSTS = []
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#
+# DATABASE
+#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -14,6 +20,7 @@ DATABASES = {
     }
 }
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+#
+# STATIC FILES
+#
 STATIC_URL = '/static/'
